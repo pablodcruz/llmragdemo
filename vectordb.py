605231a2f1d9919893a367af81e8eafd0df76ad8
pablodcruz <pablo.delacruz@revature.com>
2024-01-24 17:32:39 -0500

    removed transformer code

--- vectordb.py
+++ vectordb.py
@@ -1,11 +1,6 @@
 import chromadb
 import PyPDF2
-from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-def embed_text(text):
-    return model.encode(text)
 
 # Local db mode
 client = chromadb.PersistentClient(path="./")
